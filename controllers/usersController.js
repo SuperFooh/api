009552fa98227675dd02d4809a359fc2ba8581b4
Manuel Ibar <mibar1304@gmail.com>
2020-06-24 10:49:31 -0300

    seteadas las variables de entonrno para definir puertos dinamicos

--- controllers/usersController.js
+++ controllers/usersController.js
@@ -1,10 +1,10 @@
 const User = require('../models/UserModel')
 const ObjectId = require('mongoose').Types.ObjectId
-const 
+
 const UserController = (req, res, next) => {
     
     const { name, nickName, email, rawPassword } = req.body
-    const 
+
     setUser: () => {
         new User({
             _id: new ObjectId(),
